ef3a79dfcbd13e561938e3cbfc5d184c9366b231
Hkaler <39790017+Hkaler@users.noreply.github.com>
2019-04-08 00:32:41 -0600

    Lined up entry TextBoxes

--- admin/animals/create-animal.php
+++ admin/animals/create-animal.php
@@ -45,19 +45,33 @@ if (isset($_POST['submit'])) {
 	
 	<form method="post">
 		<label for="TagNo">Tag Number</label>
+		<br />
 		<input type="number" name="TagNo" id="TagNo">
+		<br />
 		<label for="Weight">Weight</label>
+		<br />
 		<input type="number" name="Weight" id="Weight">
+		<br />
 		<label for="Species">Species</label>
+		<br />
 		<input type="text" name="Species" id="Species">
+		<br />
 		<label for="Gender">Gender</label>
+		<br />
 		<input type="text" name="Gender" id="Gender">
+		<br />
 		<label for="Type">Type</label>
+		<br />
 		<input type="text" name="Type" id="Type">
+		<br />
 		<label for="Hunter">Hunter #</label>
+		<br />
 		<input type="number" name="Hunter" id="Hunter">
+		<br />
 		<label for="WeaponUsed">Weapon Used</label>
+		<br />
 		<input type="text" name="WeaponUsed" id="WeaponUsed">
+		<br />
 		<input type="submit" name="submit" value="Submit">
 	</form>
 </body>
