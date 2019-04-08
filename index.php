eb9e5eb49284071863e269e1cb548f49fc8d597b
Eric Austin <ericaustin@Erics-MacBook-Pro.local>
2019-04-08 16:22:43 -0600

    index is now login page

--- index.php
+++ index.php
@@ -1,17 +1,57 @@
+
+<?php 
+
+/**
+ * use an html form to login
+ * 
+ */
+
+
+
+if (isset($_POST['submit'])) {
+    require "config.php";
+    require "common.php";
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+        
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        $sql = "SELECT *
+                FROM Login
+                WHERE username = :username AND password = :password";
+        
+        $statement = $connection->prepare($sql);
+        $statement->bindValue(":username", $username);
+        $statement->bindValue(":password", $password);
+        $statement->execute();
+        
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $error) {
+        echo $sql . "<br/>" . $error->getMessage();
+    }
+    
+}
+
+?>
+
+
 <?php include "templates/header.php" ?>
 
+
+
 <body>
-	<h1>Choose Login</h1>
+	<h1>Login</h1>
 	
-	<ul>
-		<li>
-			<a href="member/member-login.php"><strong>Member Login</strong></a>
-		</li>
-		<li>
-			<a href="admin/admin-login.php"><strong>Admin Login</strong></a>
-		</li>
-	</ul>
+	<?php if (isset($_POST['submit']) && $statement) { ?>
+  		<?php echo escape($result["MemberID"]); ?> successfully logged in.
+	<?php } ?>
 	
+	<form method="post">
+		Username: <br /><input type="text" name="username" id="username"/><br />
+		Password: <br /><input type="text" name="password" id="password"/><br />
+		<input type="submit" name="submit" value="Submit"> 
+	</form>
 </body>
 
 <?php include "templates/footer.php" ?>
